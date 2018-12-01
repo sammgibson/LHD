ffb0a278a96f9562b6318098f1d22eb94ea5b9ea
Samuel Gibson <SmmGibson@eduroam-169-233-137-250.ucsc.edu>
2018-12-01 10:50:34 -0800

    Added library.

--- webscraper.py
+++ webscraper.py
@@ -4,10 +4,20 @@
 # Konstantyn Shumishyn
 # Samuel Gibson
 # 12/1/18
+from requests import get
+from request.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+
+def simple_get(url):
+    print
+
+
 
 def main():
-    print " Webscrape away"
-    
+    url = input("Please enter somthing.")
+
 
 if __name__ == "__main__":
     main()
