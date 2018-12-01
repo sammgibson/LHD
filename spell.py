2ca818eb2fabec3d30e5724331f36e5be9c3aaee
Samuel Gibson <SmmGibson@eduroam-169-233-137-250.ucsc.edu>
2018-12-01 10:18:39 -0800

    First python functions.

--- spell.py
+++ spell.py
@@ -1,3 +1,22 @@
+#Drop duplicate adjacent letters, except for C.
+def procedureOne():
+
+#If the word begins with "KN", "GN" "PN" "AE" "WR" drop the first letter
+def procedureTwo():
+
+#Drop "B" if after "M" at the end of the word
+def procedureThree():
+
+#"C" transforms to "X" if followed by "IA: or "H"(Unless in latter, it is part of "-SCH-" in which case it transforms to "K"). "C" transforms to "S" if followed by "I", "E" or :Y" Otherwise, "C" transforms to "K"
+def procedureFour():
+
+
+#"D" transforms to "J" if followed by "GE" "GY" or "GI" otherwise "D" transforms to "T"
+def procedureFive():
+
+
+    
+
 
 
 def spellChecker():
