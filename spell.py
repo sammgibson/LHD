94ef593a076b0a20c4068129d6ec21152f001654
Samuel Gibson <SmmGibson@eduroam-169-233-137-250.ucsc.edu>
2018-12-01 10:04:23 -0800

    First python functions.

---
+++ spell.py
@@ -0,0 +1,15 @@
+
+
+def spellChecker():
+    print "This is our basic function."
+    print "we need to get a dictionary with common words."
+    print "We will probably need to web scrap."
+    print "Unless Justin has some great idea."
+
+def main():
+    spellChecker()
+
+
+if __name__ == "__main__":
+    main()
+
